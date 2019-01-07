9ad05863c038b0fbd1e458080bf0d738b9b5b699
Ilia Sheiko <ilia.sheiko@profinit.eu>
2019-01-07 14:18:05 +0100

    kafka-python - experiments 0

--- tools/KafkaConsumer.py
+++ tools/KafkaConsumer.py
@@ -1,9 +1,22 @@
+import json
+from time import sleep
+
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer(bootstrap_servers='ip-172-31-6-28.ec2.internal:9092,'
-                                           'ip-172-31-11-76.ec2.internal:9092,'
-                                           'ip-172-31-5-160.ec2.internal:9092')
-consumer.subscribe(['topic'])
-for msg in consumer:
-    print(msg.headers)
-    print(msg)
+if __name__ == '__main__':
+    topic_name = 'test'
+
+    consumer = KafkaConsumer(topic_name,
+                             auto_offset_reset='earliest',
+                             consumer_timeout_ms=1000,
+                             bootstrap_servers=['ip-172-31-6-28.ec2.internal:9092',
+                                                'ip-172-31-11-76.ec2.internal:9092,',
+                                                'ip-172-31-5-160.ec2.internal:9092'])
+    for msg in consumer:
+        print(msg.value)
+
+
+        sleep(3)
+
+    # if consumer is not None:
+    #     consumer.close()
--- tools/KafkaProducer.py
+++ tools/KafkaProducer.py
@@ -2,14 +2,36 @@ import datetime
 
 from kafka import KafkaProducer
 
-p = KafkaProducer(bootstrap_servers='ip-172-31-6-28.ec2.internal:9092,'
-                                    'ip-172-31-11-76.ec2.internal:9092,'
-                                    'ip-172-31-5-160.ec2.internal:9092')
+
+def connect_kafka_producer():
+    _producer = None
+    try:
+        _producer = KafkaProducer(bootstrap_servers=['ip-172-31-6-28.ec2.internal:9092',
+                                                     'ip-172-31-11-76.ec2.internal:9092,',
+                                                     'ip-172-31-5-160.ec2.internal:9092'])
+    except Exception as ex:
+        print('Exception while connecting Kafka')
+        print(str(ex))
+    finally:
+        return _producer
+
+
+def publish_message(producer_instance, topic_name, value):
+    try:
+        # key_bytes = bytes(key, encoding='utf-8')
+        value_bytes = bytes(value, encoding='utf-8')
+        producer_instance.send(topic_name, value=value_bytes)
+        producer_instance.flush()
+        print('Message published successfully.')
+    except Exception as ex:
+        print('Exception in publishing message: %e' % ex)
+
 
 some_data_source = []
 for i in range(20):
     new_Value = str(i) + " " + str(datetime.datetime.now())
     some_data_source.append(new_Value)
 
+p = connect_kafka_producer()
 for data in some_data_source:
-        p.send('test', data.encode('utf-8'))
+    publish_message(p, 'test', data)
