13924a62c3ff37e5f93aae20b08ae81b571192db
Ilia Sheiko <ilia.sheiko@profinit.eu>
2019-01-08 17:30:23 +0100

    Fixes 7

--- SocketServer.py
+++ SocketServer.py
@@ -6,7 +6,7 @@ def get_socket_server(port):
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		print("Socket successfully created")
-		s.bind('localhost', port)
+		s.bind(('localhost', int(port)))
 		s.listen()
 		print("socket binded to %s" % (port))
 	except socket.error as err:
@@ -27,6 +27,6 @@ def try_get_socket_message(server):
 	
 def send_socket_message(host, port, message):
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((host, port))
+		s.connect((host, int(port)))
 		s.sendall(pickle.dumps(message))
 		data = s.recv(1024)
